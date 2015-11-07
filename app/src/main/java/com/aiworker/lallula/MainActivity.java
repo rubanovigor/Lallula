da0bf34babada8a8860ae379af753231312affdb
rubanovigor <rubanovigor@gmail.com>
2015-11-07 21:11:09 +0200

    adding sharing button

--- app/src/main/java/com/aiworker/lallula/MainActivity.java
+++ app/src/main/java/com/aiworker/lallula/MainActivity.java
@@ -1,9 +1,15 @@
 package com.aiworker.lallula;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,24 +23,39 @@ import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.Profile;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+import com.facebook.share.ShareApi;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class MainActivity extends AppCompatActivity {
-    public static TextView info, tv_info;
+    public static TextView tv_facebookInfo, tv_AppStat;
     private LoginButton loginButton;
+    ProfilePictureView profilePicture;
     private CallbackManager callbackManager;
-    Button statsBtn;
-    private String tempM="";
+//    private LoginManager loginManager;
+    Button btn_AppStat, btn_facebookShare;
+    ShareDialog shareDialog;
+
+//    private static final List<String> PERMISSIONS = Arrays.asList("publish_actions");
+//    private String tempM="";
 
 
     @Override
@@ -44,16 +65,31 @@ public class MainActivity extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
 
+        /** You need this method to be used only once to configure
+        your key hash in your App Console at developers.facebook.com/apps */
+        getFbKeyHash("com.aiworker.lallula");
+
         /** declare layout and corresponding elements */
         setContentView(R.layout.activity_main);
 
-        info = (TextView)findViewById(R.id.info);
-        tv_info = (TextView)findViewById(R.id.tv_info);
+        tv_facebookInfo = (TextView)findViewById(R.id.tv_facebookInfo);
+        tv_AppStat = (TextView)findViewById(R.id.tv_AppStat);
+        btn_AppStat = (Button) findViewById(R.id.btn_AppStat);
+        btn_facebookShare = (Button) findViewById(R.id.btn_facebookShare);
         loginButton = (LoginButton)findViewById(R.id.login_button);
+        profilePicture = (ProfilePictureView)findViewById(R.id.profile_picture);
 
-        //loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday"));
-        loginButton.setReadPermissions(Arrays.asList("user_birthday"));
+        shareDialog = new ShareDialog(this);
 
+        loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday"));
+//        loginManager = LoginManager.getInstance();
+//        loginManager.logInWithPublishPermissions(this, PERMISSIONS);
+
+        /** get app usage statistics and displayed it in TextView */
+        btn_AppStat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {UStats.printCurrentUsageStatus(MainActivity.this); }
+        });
 
         /** Check if permission enabled */
         if (UStats.getUsageStatsList(this).isEmpty()){
@@ -61,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
 
+        profilePicture.setProfileId(null);
         /** register the custom callback */
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -84,7 +121,9 @@ public class MainActivity extends AppCompatActivity {
                                     String gender = object.getString("gender");
                                     String birthday = object.getString("birthday");
                                     System.out.println(id + ", " + name + ", " + email + ", " + gender + ", " + birthday);
-                                    info.setText(birthday);
+//                                    tv_facebookInfo.setText(birthday);
+                                    tv_facebookInfo.setText("ID: " + id + " | " + email + "\n" + name + ", " + " | " + gender + " | " + birthday);
+                                    profilePicture.setProfileId(object.getString("id"));
                                 }
                                 catch (JSONException e)
                                 {
@@ -94,31 +133,25 @@ public class MainActivity extends AppCompatActivity {
                             }
                         });
                 Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,email,gender, birthday");
+                parameters.putString("fields", "id, name, email, gender, birthday, picture");
                 request.setParameters(parameters);
                 request.executeAsync();
             }
 
             @Override
             public void onCancel() {
-                info.setText("Login attempt canceled.");
+                tv_facebookInfo.setText("Login attempt canceled.");
             }
 
             @Override
             public void onError(FacebookException e) {
-                info.setText("Login attempt failed.");
+                tv_facebookInfo.setText("Login attempt failed.");
             }
 
         });
 
-        /** check if user already login, get profile info if yes */
-        //boolean loggedIn=false;
+        /** check if user already login, get and display profile info if yes */
         if(AccessToken.getCurrentAccessToken()!=null){
-          //  loggedIn=true;
-           // Profile actualProfile = Profile.getCurrentProfile();
-            //String name = actualProfile.getName();
-            //info.setText(name);
-
             GraphRequest request = GraphRequest.newMeRequest(
                     AccessToken.getCurrentAccessToken(),
                     new GraphRequest.GraphJSONObjectCallback() {
@@ -135,37 +168,88 @@ public class MainActivity extends AppCompatActivity {
                                 String gender = object.getString("gender");
                                 String birthday = object.getString("birthday");
                                 System.out.println(id + ", " + name + ", " + email + ", " + gender + ", " + birthday);
-                                info.setText(birthday);
+//                                tv_facebookInfo.setText(birthday);
+                                tv_facebookInfo.setText("ID: " + id + " | " + email + "\n" + name + ", " + " | " + gender + " | " + birthday);
+                                profilePicture.setProfileId(object.getString("id"));
                             }
                             catch (JSONException e)
                             {
                                 e.printStackTrace();
-                                info.setText("fuck FB");
+                                tv_facebookInfo.setText("exception...");
                             }
                         }
                     });
             Bundle parameters = new Bundle();
-            parameters.putString("fields", "id,name,email,gender, birthday");
+            parameters.putString("fields", "id,name,email,gender, birthday, picture");
             request.setParameters(parameters);
             request.executeAsync();
 
+        } else {
+            profilePicture.setProfileId(null);
+            tv_facebookInfo.setText("please login");
         }
 
-        /** get app usage statistics and displayed it in TextView */
-        statsBtn = (Button) findViewById(R.id.stats_btn);
-        statsBtn.setOnClickListener(new View.OnClickListener() {
+        /** display sharing dialog */
+        btn_facebookShare.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {UStats.printCurrentUsageStatus(MainActivity.this); }
+            public void onClick(View view) {
+                ShareLinkContent content = new ShareLinkContent.Builder().build();
+                shareDialog.show(content);
+
+            }
         });
 
 
     }
 
 
+    private void sharePhotoToFacebook(){
+        Bitmap image = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        SharePhoto photo = new SharePhoto.Builder()
+                .setBitmap(image)
+                .setCaption("Give me my code or I will ... you know, do that thing you don't like!")
+                .build();
+
+        SharePhotoContent content = new SharePhotoContent.Builder()
+                .addPhoto(photo)
+                .build();
+
+        ShareApi.share(content, null);
+
+        tv_AppStat.setText("sharing");
+    }
+
+    public void getFbKeyHash(String packageName) {
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    packageName,
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("YourKeyHash :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                System.out.println("YourKeyHash: "+ Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+
+
     }
 
 }
--- app/src/main/java/com/aiworker/lallula/UStats.java
+++ app/src/main/java/com/aiworker/lallula/UStats.java
@@ -17,6 +17,7 @@ import java.util.List;
 public class UStats {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("M-d-yyyy HH:mm:ss");
     public static final String TAG = UStats.class.getSimpleName();
+    public static long endTime, startTime;
 
     @SuppressWarnings("ResourceType")
    /* public static void getStats(Context context){
@@ -44,30 +45,43 @@ public class UStats {
     public static List<UsageStats> getUsageStatsList(Context context){
         UsageStatsManager usm = getUsageStatsManager(context);
         Calendar calendar = Calendar.getInstance();
-        long endTime = calendar.getTimeInMillis();
-        calendar.add(Calendar.YEAR, -1);
-        long startTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.YEAR, -0);
+        endTime = calendar.getTimeInMillis();
+        calendar.add(Calendar.YEAR, +0);
+
+        calendar.add(Calendar.YEAR, -2);
+//        calendar.add(Calendar.MONTH, -1);
+//        calendar.add(Calendar.HOUR_OF_DAY, -24);
+//        calendar.add(Calendar.HOUR_OF_DAY, -1);
+        startTime = calendar.getTimeInMillis();
 
         Log.d(TAG, "Range start:" + dateFormat.format(startTime) );
         Log.d(TAG, "Range end:" + dateFormat.format(endTime));
 
-        List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,startTime,endTime);
+//        final List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_BEST,startTime,endTime);
+//        final List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,startTime,endTime);
+//                List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_WEEKLY,startTime,endTime);
+//        List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_MONTHLY,startTime,endTime);
+         List<UsageStats> usageStatsList = usm.queryUsageStats(UsageStatsManager.INTERVAL_YEARLY,startTime,endTime);
+//        List<UsageStats> usageStatsList = usm.queryAndAggregateUsageStats(startTime,endTime);
         return usageStatsList;
     }
 
     public static void printUsageStats(List<UsageStats> usageStatsList){
-        String app_stats_info = "Foreground time in ms/min\n";
+        String app_stats_info = "period: " + dateFormat.format(startTime) + " - " + dateFormat.format(endTime) + "\n" + "app's foreground time in ms/min\n";
         for (UsageStats u : usageStatsList){
-            Log.d(TAG, "Pkg: " + u.getPackageName() +  "\t" + "ForegroundTime: "
+            Log.d(TAG, "Pkg: " + u.getPackageName() + "\t" + "ForegroundTime: "
                     + u.getTotalTimeInForeground()) ;
 
-            app_stats_info = app_stats_info +
-                    "Pkg: " + u.getPackageName() +  "\t" +
-                    ": " + u.getTotalTimeInForeground() + " s/" + Math.round(u.getTotalTimeInForeground()/60000) + " m\n";
-
+            if(u.getPackageName().contentEquals("com.facebook.orca") || u.getPackageName().contentEquals("com.facebook.katana")) {
+//            if(u.getPackageName().contentEquals("com.augmentra.viewranger.android")) {
+                app_stats_info = app_stats_info +
+                        "Pkg: " + u.getPackageName() + "\t" +
+                        ": " + u.getTotalTimeInForeground() + " s/" + Math.round(u.getTotalTimeInForeground() / 60000) + " m\n";
+            }
         }
 
-        MainActivity.tv_info.setText(app_stats_info);
+        MainActivity.tv_AppStat.setText(app_stats_info);
     }
 
 
